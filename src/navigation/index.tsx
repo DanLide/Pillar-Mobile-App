2332753ba9192d06feb014efd3b9b9301ec26ce7
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-03-29 23:18:39 +0300

    refactor: move screen title to separate options const

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -7,7 +7,7 @@ import { authStore } from '../stores';
 
 import { LoginScreen } from '../modules/login/components/LoginScreen';
 import { HomeScreen } from '../modules/home/HomeScreen';
-import TermsScreen from '../modules/login/components/TermsScreen';
+import TermsScreen from '../modules/terms/TermsScreen';
 
 enum AppNavigator {
   HomeScreen = 'HomeScreen',
@@ -17,33 +17,53 @@ enum AppNavigator {
 
 const Stack = createStackNavigator();
 
+type ScreenOptions = React.ComponentProps<
+  typeof Stack.Navigator
+>['screenOptions'];
+
+const termsScreenOptions: ScreenOptions = {
+  title: 'Terms & Conditions',
+};
+
 export const AppStack = observer(() => {
   const store = useRef(authStore).current;
 
   const currentScreen = useMemo(() => {
-    if (!store.isLoggedIn) {
-      return (
-        <Stack.Screen
-          name={AppNavigator.LoginScreen}
-          component={LoginScreen}
-          options={defaultOptions}
-        />
-      );
-    }
-
-    if (!store.getIsTnC) {
-      return (
-        <Stack.Screen name={AppNavigator.TermsScreen} component={TermsScreen} />
-      );
-    }
-
     return (
       <Stack.Screen
-        name={AppNavigator.HomeScreen}
-        component={HomeScreen}
-        options={defaultOptions}
+        name={AppNavigator.TermsScreen}
+        component={TermsScreen}
+        options={termsScreenOptions}
       />
     );
+
+    // if (!store.isLoggedIn) {
+    //   return (
+    //     <Stack.Screen
+    //       name={AppNavigator.LoginScreen}
+    //       component={LoginScreen}
+    //       options={defaultOptions}
+    //     />
+    //   );
+    // }
+    //
+    // if (!store.getIsTnC) {
+    //   return (
+    //     <Stack.Screen
+    //       name={AppNavigator.TermsScreen}
+    //       component={TermsScreen}
+    //       options={termsScreenOptions}
+    //     />
+    //   );
+    // }
+    //
+    // return (
+    //   <Stack.Screen
+    //     name={AppNavigator.HomeScreen}
+    //     component={HomeScreen}
+    //     options={defaultOptions}
+    //   />
+    // );
   }, [store.getIsTnC, store.isLoggedIn]);
 
   return <Stack.Navigator>{currentScreen}</Stack.Navigator>;
