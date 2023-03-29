2f116c04fed90a09672874efd3147bfdbc4ef2d1
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-03-29 23:18:08 +0300

    feat: add terms submit button

--- src/modules/terms/TermsScreen.tsx
+++ src/modules/terms/TermsScreen.tsx
@@ -3,10 +3,12 @@ import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { handleExternalLinkInBrowser, SOURCE_DEFAULT } from './helpers/webview';
 import useSwitchState from '../../hooks/useSwitchState';
-import Switch from '../../components/Switch';
+import { Switch, Button } from '../../components';
 
 const TermsScreen: React.FC = () => {
   const [isTermsAccepted, toggleTermsAccepted] = useSwitchState();
+
+  const isSubmitDisabled = !isTermsAccepted;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -20,6 +22,11 @@ const TermsScreen: React.FC = () => {
           onPress={toggleTermsAccepted}
           label="I agree to Terms"
         />
+        <Button
+          title="Submit"
+          disabled={isSubmitDisabled}
+          buttonStyle={styles.submitButton}
+        />
       </View>
     </SafeAreaView>
   );
@@ -27,6 +34,7 @@ const TermsScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  controlsContainer: { padding: 16 },
+  controlsContainer: { padding: 16, paddingBottom: 0 },
+  submitButton: { marginTop: 8 },
 });
 export default memo(TermsScreen);
