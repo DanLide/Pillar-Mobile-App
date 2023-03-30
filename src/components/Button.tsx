9ba70f8f1830cd57a4b5f89f44bfebc4f2af08e4
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-03-30 05:13:53 +0300

    feat: add loading indicator for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from 'react';
 import {
   StyleSheet,
   TouchableOpacity,
@@ -6,41 +6,62 @@ import {
   ViewStyle,
   TextStyle,
   TextProps,
-} from "react-native";
-import Text from "./Text";
+  ActivityIndicator,
+  StyleProp,
+} from 'react-native';
+import Text from './Text';
 
 type ButtonProps = TouchableOpacityProps & TextProps;
+
 interface ExtendedButtonProps extends ButtonProps {
   title: string;
+  isLoading?: boolean;
   buttonStyle?: ViewStyle;
   textStyle?: TextStyle;
 }
 
 const Button: React.FC<ExtendedButtonProps> = ({
   title,
+  isLoading,
   buttonStyle,
   textStyle,
   disabled,
   ...props
 }) => {
+  const isDisabled = isLoading || disabled;
+
+  const buttonMergedStyle = useMemo<StyleProp<ViewStyle>>(
+    () => [styles.button, isDisabled && styles.disabledStyle, buttonStyle],
+    [buttonStyle, isDisabled],
+  );
+
+  const textMergedStyle = useMemo<StyleProp<TextStyle>>(
+    () => [styles.buttonText, textStyle],
+    [textStyle],
+  );
+
   return (
     <TouchableOpacity
       {...props}
-      disabled={disabled}
-      style={[styles.button, disabled && styles.disabledStyle, buttonStyle]}
+      disabled={isDisabled}
+      style={buttonMergedStyle}
     >
-      <Text {...props} style={[styles.buttonText, textStyle]}>
-        {title}
-      </Text>
+      {isLoading ? (
+        <ActivityIndicator size="small" color="white" />
+      ) : (
+        <Text {...props} style={textMergedStyle}>
+          {title}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: 50,
     borderRadius: 16,
   },
@@ -49,7 +70,7 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 17,
-    color: "white",
+    color: 'white',
   },
 });
 
