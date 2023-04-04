a5f31e6dff130c7288fc93db375f36eb5b76f525
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-04-04 12:30:09 +0300

    refactor: remove user permission check on Home screen

--- src/modules/home/HomeScreen.tsx
+++ src/modules/home/HomeScreen.tsx
@@ -3,18 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import { Button } from '../../components';
 import { onGetRoleManager } from '../../data/getRoleManager';
 import { authStore } from '../../stores';
-import { hasAllPermissions } from '../../data/helpers';
-import { Permission } from '../../constants';
-
-const MANAGE_ORDERS_PERMISSIONS: Permission[] = [
-  Permission.InventoryManagement_OrderMobile_View,
-  Permission.InventoryManagement_OrderMobile_Create,
-  Permission.InventoryManagement_OrderMobile_Edit,
-];
 
 export const HomeScreen: React.FC = () => {
-  const userHasOrderPermission = hasAllPermissions(MANAGE_ORDERS_PERMISSIONS);
-
   const onGetRoleManagerPress = async () => {
     const error = await onGetRoleManager('token');
     console.log(error, 'error');
@@ -31,10 +21,6 @@ export const HomeScreen: React.FC = () => {
         title="Try broken getRoleManager"
         onPress={onGetRoleManagerPress}
       />
-
-      {userHasOrderPermission && (
-        <Button buttonStyle={styles.button} title="Manage orders" />
-      )}
 
       <Button buttonStyle={styles.button} title="Logout" onPress={onLogout} />
     </View>
