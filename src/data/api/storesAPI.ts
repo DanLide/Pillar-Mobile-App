67e9fdb5136bfb6c2babc16fb6625e0cfc0c114d
Dmitriy Morgunov CW <ADRDZZZ@mmm.com>
2023-04-06 10:49:17 +0300

    Remove unused code

--- src/data/api/storesAPI.ts
+++ src/data/api/storesAPI.ts
@@ -5,9 +5,5 @@ import { Stock } from '../../modules/stocksList/stores/StocksStore';
 export const getStoresAPI = (facilityId: number, partyRoleID: number) => {
   const url = new URLProvider().getStocksUrl(facilityId, partyRoleID);
 
-  const body = JSON.stringify([
-    { id: PartySettingsType.IsTermsAccepted, value: true },
-  ]);
-
   return tryAuthFetch<Stock[]>({ url, request: { method: 'GET' } });
 };
