4a035a9827707b1d6a8e1d1d89e8a05a6e5164bf
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-04-10 15:54:43 +0300

    test: add error test case for login task

--- src/data/__tests__/login.ts
+++ src/data/__tests__/login.ts
@@ -2,26 +2,43 @@ import { LoginFlowContext, LoginTask } from '../login';
 import * as LoginAPI from '../api/login';
 
 describe('login', () => {
-  const loginContext: LoginFlowContext = {};
-
   it('should execute login task', async () => {
-    const mockedLoginAPI = jest
+    const loginContext: LoginFlowContext = {};
+
+    const mockedLoginSuccess = jest
       .spyOn(LoginAPI, 'loginAPI')
-      .mockReturnValue(Promise.resolve(loginSuccessResponse));
+      .mockReturnValue(Promise.resolve(LOGIN_RESPONSE));
 
-    await new LoginTask(loginContext, loginParams).run();
+    const loginTask = new LoginTask(loginContext, LOGIN_PARAMS);
 
+    await expect(loginTask.run()).resolves.not.toThrowError();
     expect(loginContext.token).toBeDefined();
-    expect(mockedLoginAPI).toBeCalled();
+    expect(mockedLoginSuccess).toBeCalled();
+  });
+
+  it('should throw error on login task', async () => {
+    const loginContext: LoginFlowContext = {};
+
+    const mockedLoginError = jest
+      .spyOn(LoginAPI, 'loginAPI')
+      .mockImplementation(() => {
+        throw new Error();
+      });
+
+    const loginTask = new LoginTask(loginContext, LOGIN_PARAMS);
+
+    await expect(loginTask.run()).rejects.toThrowError();
+    expect(loginContext.token).toBeUndefined();
+    expect(mockedLoginError).toBeCalled();
   });
 });
 
-const loginParams: LoginAPI.LoginAPIParams = {
+const LOGIN_PARAMS: LoginAPI.LoginAPIParams = {
   username: 'test',
   password: 'test',
 };
 
-const loginSuccessResponse: LoginAPI.LoginAPIResponse = {
+const LOGIN_RESPONSE: LoginAPI.LoginAPIResponse = {
   access_token: 'access_token',
   expires_in: 86400,
   refresh_token: 'refresh_token',
