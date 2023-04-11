022bb0c21c52bbc38bb9afa349927dff68d13bbb
Andrii Nahornyi <andrii.nahornyi@globallogic.com>
2023-04-11 09:07:09 +0300

    fix(components): replace camera library

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -1,29 +1,61 @@
-import React, { type PropsWithChildren, useState } from 'react';
-import { View, ViewStyle, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import QRCodeScanner, { RNQRCodeScannerProps } from 'react-native-qrcode-scanner'
-import { RNCamera } from 'react-native-camera'
-
-interface ScannerProps extends RNQRCodeScannerProps {
+import React, { useEffect } from 'react';
+import { View, ViewStyle, Text, StyleSheet, TouchableOpacity, Alert, ActivityIndicator } from 'react-native'
+import { useScanBarcodes, BarcodeFormat, Barcode } from 'vision-camera-code-scanner';
+import { useCameraDevices, Camera, CameraProps } from 'react-native-vision-camera';
+interface ScannerProps extends CameraProps {
   isCamera: boolean
   containerStyle?: ViewStyle
+  onRead?: (barcode: string) => void
   onPressClose: () => void
 }
 
-const Scanner: React.FC<ScannerProps> = ({ isCamera, containerStyle, onPressClose, ...props }) => {
+const Scanner: React.FC<ScannerProps> = ({ isCamera, containerStyle, onPressClose, onRead, ...props }) => {
+  const devices = useCameraDevices();
+  const device = devices.back;
 
-  const renderCamera = isCamera ?
-    <QRCodeScanner
-      {...props}
-      flashMode={RNCamera.Constants.FlashMode.torch}
-      focusDepth={1}
-    /> :
-    null
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.ALL_FORMATS]);
+
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [isScanned, setIsScanned] = React.useState(false);
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  const checkCameraPermission = async () => {
+    const status = await Camera.getCameraPermissionStatus();
+    setHasPermission(status === 'authorized');
+  };
+
+  useEffect(() => {
+    toggleActiveState();
+  }, [barcodes]);
+
+  const toggleActiveState = () => {
+    if (barcodes.length && isScanned === false) {
+      //setIsScanned(true)
+      barcodes.forEach((scannedBarcode: Barcode) => {
+        if (scannedBarcode.rawValue) {
+          onRead && onRead(scannedBarcode.rawValue);
+        }
+      });
+    }
+  };
+
+  if (!device) return <ActivityIndicator />;
 
   return <View style={containerStyle}>
     <TouchableOpacity style={styles.buttonContainer} onPress={onPressClose}>
       <Text style={styles.close}>X</Text>
     </TouchableOpacity>
-    {renderCamera}
+    <Camera
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+      frameProcessorFps={5}
+      audio={false}
+      {...props}
+    />
   </View>
 }
 
@@ -38,7 +70,7 @@ const styles = StyleSheet.create({
   close: {
     fontSize: 20,
     padding: 5,
-  }
+  },
 });
 
 export default Scanner
