9df9fbf4cadceca753d73eb242e003cec5b8571b
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-04-14 12:50:35 +0300

    feat: replace accept terms mock with real API call

--- src/data/api/acceptTerms.ts
+++ src/data/api/acceptTerms.ts
@@ -1,14 +1,12 @@
-// import { URLProvider, tryAuthFetch } from '../helpers';
-// import { PartySettingsType } from '../../constants';
+import { URLProvider, tryAuthFetch } from '../helpers';
+import { PartySettingsType } from '../../constants';
 
 export const acceptTermsAPI = () => {
-  // TODO: uncomment when API fix is released
-  // const url = new URLProvider().getAcceptTermsUrl();
-  //
-  // const body = JSON.stringify([
-  //   { id: PartySettingsType.IsTermsAccepted, value: true },
-  // ]);
-  //
-  // return tryAuthFetch<Response>({ url, request: { method: 'PUT', body } });
-  return new Promise<void>(resolve => setTimeout(resolve, 1000));
+  const url = new URLProvider().getAcceptTermsUrl();
+
+  const body = JSON.stringify([
+    { id: PartySettingsType.IsTermsAccepted, value: true },
+  ]);
+
+  return tryAuthFetch<Response>({ url, request: { method: 'PUT', body } });
 };
