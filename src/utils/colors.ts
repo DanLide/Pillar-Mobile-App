0411bb7a482c90bdb232a45feefad991c87f6a5e
Dmitriy Morgunov CW <ADRDZZZ@mmm.com>
2023-04-20 16:29:29 +0300

    Add fonts and colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  purpleDark: '#7634BC',
+  purple: '#9657D9',
+  purpleLight: '#DBC2F9',
+  white: '#FFFFFF',
+  black: '#000000',
+  blackSemiLight: '#323237',
+  blackLight: '#424247',
+  grayDark: '#95959E',
+  gray: '#E1E1E8',
+  grayLight: '#F8F8FA',
+};
