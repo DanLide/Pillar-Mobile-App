b90c479a7ba5bba30acc2b7e4003571fc7c37125
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-04-27 11:01:07 +0300

    refactor: put TODO for hardcoded magic link

--- src/modules/login/components/LoginWithPIN.tsx
+++ src/modules/login/components/LoginWithPIN.tsx
@@ -5,6 +5,7 @@ import TokenParser, { TokenParserProps } from './TokenParser';
 
 export type LoginWithPINProps = Pick<TokenParserProps, 'onTokenReceived'>;
 
+// TODO: remove this when magic link generation is implemented
 const MAGIC_LINK =
   'https://3maaddev.b2clogin.com/3maaddev.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_Auth_With_UserId&client_id=5a060590-680c-4253-96c7-dd2adcfbddaf&nonce=defaultNonce&redirect_uri=https%3a%2f%2fjwt.ms&scope=openid&response_type=id_token&prompt=login&user_id=e81dd3dc-9317-4b1d-aa21-af92af55dfa4';
 
