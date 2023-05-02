d861d45b1ca13ceb5d3486b39860906650e82c53
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-05-02 22:27:19 +0300

    style: add missing line

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -26,6 +26,7 @@ const Input: React.FC<Props> = ({
     () => [styles.container, containerStyle],
     [containerStyle],
   );
+
   const inputStyle = useMemo<StyleProp<TextStyle>>(
     () => [styles.input, style],
     [style],
