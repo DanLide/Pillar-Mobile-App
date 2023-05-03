374d2be7ac2681232913318f35fe548068a3b443
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-05-03 16:34:11 +0300

    chore: add react-native-toast-message

--- App.tsx
+++ App.tsx
@@ -1,22 +1,26 @@
-import React from "react";
-import "react-native-url-polyfill/auto";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from 'react';
+import 'react-native-url-polyfill/auto';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Toast from 'react-native-toast-message';
 
-import { AppStack } from "./src/navigation";
+import { AppStack } from './src/navigation';
 
 interface InitialProps {
-   [key: string]: string;
+  [key: string]: string;
 }
 
-const App = (initialProps:InitialProps) => {
+const App = (initialProps: InitialProps) => {
   console.log(initialProps['rntoken']);
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <AppStack />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppStack />
+        </NavigationContainer>
+      </SafeAreaProvider>
+      <Toast />
+    </>
   );
 };
 
