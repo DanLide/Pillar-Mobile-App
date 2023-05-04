4bfd0112b7a04578ac3e929027406490c94b9723
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-05-04 12:24:30 +0300

    chore: add react-native-toast-notifications

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Toast from 'react-native-toast-message';
 
 import { AppStack } from './src/navigation';
 
@@ -13,14 +12,11 @@ interface InitialProps {
 const App = (initialProps: InitialProps) => {
   console.log(initialProps['rntoken']);
   return (
-    <>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <AppStack />
-        </NavigationContainer>
-      </SafeAreaProvider>
-      <Toast />
-    </>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppStack />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
