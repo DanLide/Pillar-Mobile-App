7317aaf43e6f42199f54529b9cb7fcd2ae558dc5
Dmitriy Morgunov CW <ADRDZZZ@mmm.com>
2023-05-29 17:54:44 +0300

    Handle keyboard closing

--- App.tsx
+++ App.tsx
@@ -1,23 +1,37 @@
-import React from "react";
-import "react-native-url-polyfill/auto";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from 'react';
+import 'react-native-url-polyfill/auto';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ScrollView, StyleSheet } from 'react-native';
 
-import { AppStack } from "./src/navigation";
+import { AppStack } from './src/navigation';
 
 interface InitialProps {
-   [key: string]: string;
+  [key: string]: string;
 }
 
-const App = (initialProps:InitialProps) => {
+const App = (initialProps: InitialProps) => {
   console.log(initialProps['rntoken']);
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <AppStack />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.container}
+      keyboardShouldPersistTaps="never"
+      bounces={false}
+    >
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <AppStack />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
