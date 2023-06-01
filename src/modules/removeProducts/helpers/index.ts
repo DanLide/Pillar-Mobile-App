2d0194a34f6c3db6089e1c2ee770fbc98e742617
Danylo Lidzer <ADRRJZZ@mmm.com>
2023-06-01 16:05:41 +0300

    fix: sum quantity of added products with same job

--- src/modules/removeProducts/helpers/index.ts
+++ src/modules/removeProducts/helpers/index.ts
@@ -1,5 +1,11 @@
+import { addProductToList } from './addProductToList';
 import { getReservedCountById } from './getReservedCountById';
 import { groupProductsByJobId } from './groupProductsByJobId';
 import { isRemoveProductModel } from './isRemoveProductModel';
 
-export { groupProductsByJobId, isRemoveProductModel, getReservedCountById };
+export {
+  groupProductsByJobId,
+  isRemoveProductModel,
+  getReservedCountById,
+  addProductToList,
+};
