bb89050c7d10bc8d0965d821c9fd2a3209d0e826
Andrii Muzh <AG7PDZZ@mmm.com>
2024-04-15 14:56:38 +0300

    fix: remove unused code

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -79,8 +79,6 @@ const Scanner: React.FC<ScannerProps> = ({
     [onRead, isActive, isFocused],
   );
 
-  const [hasPermission, setHasPermission] = React.useState(false);
-
   !format.current &&
     device?.formats.forEach(obj => {
       if (
@@ -91,10 +89,6 @@ const Scanner: React.FC<ScannerProps> = ({
         format.current = obj;
       }
     });
-
-  useEffect(() => {
-    checkCameraPermission();
-  }, []);
 
   useEffect(() => {
     if (!layout) return;
@@ -109,11 +103,6 @@ const Scanner: React.FC<ScannerProps> = ({
 
     return () => clearInterval(timerId);
   }, [layout]);
-
-  const checkCameraPermission = async () => {
-    const status = await Camera.getCameraPermissionStatus();
-    setHasPermission(status === 'authorized');
-  };
 
   if (!device) return <ActivityIndicator />;
 
